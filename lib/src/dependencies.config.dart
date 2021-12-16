d0c507518db1f31739adb5db4f487dbb77b5acb0
leynier <leynier41@gmail.com>
2021-12-15 20:57:08 -0500

    chore: remove unused dependencies

--- lib/src/dependencies.config.dart
+++ lib/src/dependencies.config.dart
@@ -4,18 +4,17 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:dio/dio.dart' as _i6;
-import 'package:flutter/material.dart' as _i7;
+import 'package:flutter/material.dart' as _i6;
 import 'package:flutter_bloc/flutter_bloc.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:hive_flutter/hive_flutter.dart' as _i5;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:trello_to_github/src/blocs/theme/theme_bloc.dart' as _i10;
-import 'package:trello_to_github/src/dependencies.dart' as _i11;
-import 'package:trello_to_github/src/repositories/theme_repository.dart' as _i9;
+import 'package:trello_to_github/src/blocs/theme/theme_bloc.dart' as _i9;
+import 'package:trello_to_github/src/dependencies.dart' as _i10;
+import 'package:trello_to_github/src/repositories/theme_repository.dart' as _i8;
 import 'package:trello_to_github/src/utils/app_bloc_observer.dart' as _i4;
 import 'package:trello_to_github/src/utils/custom_scroll_behavior.dart'
-    as _i8; // ignore_for_file: unnecessary_lambdas
+    as _i7; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -26,13 +25,11 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
   gh.lazySingleton<_i3.BlocObserver>(() => _i4.AppBlocObserver());
   await gh.factoryAsync<_i5.Box<int>>(() => registerModule.themeBox,
       instanceName: 'theme', preResolve: true);
-  gh.factory<_i6.Dio>(() => registerModule.dio);
-  gh.lazySingleton<_i7.ScrollBehavior>(() => _i8.CustomScrollBehavior());
-  gh.factory<String>(() => registerModule.baseUrl, instanceName: 'baseUrl');
-  gh.factory<_i9.ThemeRepository>(
-      () => _i9.ThemeRepository(get<_i5.Box<int>>(instanceName: 'theme')));
-  gh.factory<_i10.ThemeBloc>(() => _i10.ThemeBloc(get<_i9.ThemeRepository>()));
+  gh.lazySingleton<_i6.ScrollBehavior>(() => _i7.CustomScrollBehavior());
+  gh.factory<_i8.ThemeRepository>(
+      () => _i8.ThemeRepository(get<_i5.Box<int>>(instanceName: 'theme')));
+  gh.factory<_i9.ThemeBloc>(() => _i9.ThemeBloc(get<_i8.ThemeRepository>()));
   return get;
 }
 
-class _$RegisterModule extends _i11.RegisterModule {}
+class _$RegisterModule extends _i10.RegisterModule {}
--- lib/src/dependencies.dart
+++ lib/src/dependencies.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
-import 'dart:developer';
 
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:trello_to_github/src/dependencies.config.dart';
 
 @InjectableInit(preferRelativeImports: false)
@@ -13,20 +10,6 @@ Future<void> configureDependencies() async => $initGetIt(GetIt.I);
 
 @module
 abstract class RegisterModule {
-  Dio get dio => Dio()
-    ..interceptors.add(
-      PrettyDioLogger(
-        logPrint: (l) => log(l.toString()),
-        compact: false,
-      ),
-    );
-
-  @Named('baseUrl')
-  String get baseUrl {
-    const defaultApiUrl = 'https://api.mesirve.app/client/v1/';
-    return const String.fromEnvironment('ApiUrl', defaultValue: defaultApiUrl);
-  }
-
   @Named('theme')
   @preResolve
   Future<Box<int>> get themeBox => Hive.openBox('theme');
